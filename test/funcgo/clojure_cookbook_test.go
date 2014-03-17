7646ad6ac5af947913b521f19e890114f9e171fd
Eamonn O'Brien-Strain <eob@well.com>
2014-03-16 17:41:53 -0700

    add tests from clojure cookbook

--- test/funcgo/clojure_cookbook_test.go
+++ test/funcgo/clojure_cookbook_test.go
@@ -2,6 +2,7 @@ package funcgo.clojure_cookbook_test
 import(
         test midje.sweet
         fgo funcgo.core
+	string clojure.string
 )
 
 func add(x,y) {
@@ -22,3 +23,48 @@ test.fact("Any function of two arguments can be written infix",
         =>,  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 )
 
+test.fact("Infix is most convenient for math operators.",
+	1 + 2,
+	=>, 3
+)
+
+test.fact("Dotted identifers are from other packages.",
+	// import section includes
+	//    string clojure.string
+	string.isBlank(""),
+	=>, true
+)
+
+test.fact("Capitalize first character in a string.",
+	string.capitalize("this is a proper sentence."),
+	=>,  "This is a proper sentence."
+)
+
+test.fact("Capitalize or lower-case all characters.",
+	string.upperCase("loud noises!"),
+	=>, "LOUD NOISES!",
+
+	string.lowerCase("COLUMN_HEADER_ONE"),
+	=>, "column_header_one",
+
+	string.lowerCase("!&$#@#%^[]"),
+	=>, "!&$#@#%^[]",
+
+	string.upperCase("Dépêchez-vous, l'ordinateur!"),
+	=>, "DÉPÊCHEZ-VOUS, L'ORDINATEUR!"
+)
+
+test.fact("Remove whitespace at beginning and end.",
+	string.trim(" \tBacon ipsum dolor sit.\n"),
+	=>, "Bacon ipsum dolor sit."
+)
+
+test.fact("Collapse whitespace into single whitespace",
+	string.replace("Who\t\nput  all this\fwhitespace here?", /\s+/, " "),
+	=>, "Who put all this whitespace here?"
+)
+
+test.fact("Windows to Unix line-endings",
+	string.replace("Line 1\r\nLine 2", "\r\n", "\n"),
+	=>, "Line 1\nLine 2"
+)
