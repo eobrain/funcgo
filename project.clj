913b6ae7c8b8a842f8ef15eb438cee45e02296ac
Eamonn O'Brien-Strain <eob@well.com>
2014-03-22 00:03:25 -0700

    add floating point literals

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcgo "0.1.12"
+(defproject funcgo "0.1.13"
   :description "Compiler from Functional Go into Clojure"
   :url "http://funcgo.com"
   :license {:name "Eclipse Public License"
